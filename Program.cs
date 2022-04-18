029f5ba9c38fb65390e18da68761bb2113351a3d
Prince Onuoha <princeonuohja3@gmail.com>
2022-04-18 22:58:08 +0100

    streefree

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            string myName = "Onuoha Prince";
+            string sex = "Male";
+            int year = 2022;
+            float month = 04;
+            double day = 18;
+             decimal wellaSchoolsession = 6;
+             string wellaSchoolstrack = "Backend";
+             string wellaAgent =  "Time to make money";
+             string wellaTutor = "Emmanuel Codease"; 
+
+             //explicit and implicit examples
+             double myAge = 29.3;
+             int myAgeint = (int)myAge;
+
+             float x = year;
+             long y = year;
+
+            Console.WriteLine ("Hello Wellahealth");
+            Console.WriteLine(myName);
+            Console.WriteLine(sex);
+            Console.WriteLine(year);
+            Console.WriteLine(month);
+            Console.WriteLine(day);
+            Console.WriteLine(wellaSchoolsession);
+            Console.WriteLine(wellaSchoolstrack);
+            Console.WriteLine(wellaAgent);
+            Console.WriteLine(wellaTutor);
+            Console.WriteLine(myAgeint);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+        }
+    }
+}
